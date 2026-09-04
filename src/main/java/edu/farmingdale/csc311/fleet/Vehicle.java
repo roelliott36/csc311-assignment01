b18d124efc6575e925c43610d5c9a0365247b3eb
Rochelle Elliott <ro.elliott36@gmail.com>
2026-09-04 00:16:06 -0400

    TODO-05: add toString, equals and hashCode (COMPLETE)

--- src/main/java/edu/farmingdale/csc311/fleet/Vehicle.java
+++ src/main/java/edu/farmingdale/csc311/fleet/Vehicle.java
@@ -236,16 +236,40 @@ public abstract class Vehicle implements Honkable {
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("TODO-05");
+        String engine = fuelType.hasEngine()
+                ? String.format("%.1fL", engineSize)
+                : "n/a";
+
+        return String.format(
+                "%d %s %s [VIN=%s] color=%s, wheels=%d, engine=%s, fuel=%s, capacity=%.1f %s",
+                year,
+                make,
+                model,
+                vin,
+                color,
+                wheels,
+                engine,
+                fuelType.getLabel(),
+                fuelCapacity,
+                fuelType.getUnit()
+        );
     }
 
     @Override
     public boolean equals(Object other) {
-        throw new UnsupportedOperationException("TODO-05");
+        if (this == other){
+            return true;
+        }
+        if (!(other instanceof Vehicle)){
+            return false;
+        }
+
+        Vehicle vehicle = (Vehicle) other;
+        return this.vin.equals(vehicle.vin);
     }
 
     @Override
     public int hashCode() {
-        throw new UnsupportedOperationException("TODO-05");
+        return vin.hashCode();
     }
 }
